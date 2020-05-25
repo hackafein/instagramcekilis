8f9cb7732cb6ee7d7a18a84e6040bc5d1fdf596d
hackafein <42136880+hackafein@users.noreply.github.com>
2020-05-25 21:10:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -55,10 +55,8 @@ def search():
             password = request.form.get("pass")
             postlink= request.form.get("postlink")
             max_limit = int(request.form.get("searching_limit"))
-
             give_away_people = ['@jakobowsky', '@someone']
             winners=Giveaway(user,password,give_away_people).send_winners(user,password,postlink,max_limit)
-           
             return render_template("cekilis.html", winners=winners)
 
 
